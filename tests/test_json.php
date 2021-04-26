6d8cbd5e6b826552a3e2530b8e94e92b2c53bf32
AdrienVieilleribiere <44771259+AdrienVieilleribiere@users.noreply.github.com>
2021-04-26 21:41:03 +0200

    Add README + Sdk renaming
    * sdk renamed
    * Create README.md

---
+++ tests/test_json.php
@@ -0,0 +1,8 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
+use Smiirl\SmiirlSdkPhp;
+$smiirlSdk = new SmiirlSdkPhp();
+
+
+$smiirlSdk->jsonUrl("424242");
+
