38db57bc71d44cc1bb02b2e7936e0a7f63e6db98
AdrienVieilleribiere <44771259+AdrienVieilleribiere@users.noreply.github.com>
2021-06-16 11:04:39 +0200

    Use new front rest routes (#4)
    * extend class to show add and reset cases
    
    * add example

--- tests/test.php
+++ tests/test.php
@@ -3,6 +3,9 @@ require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Compos
 
 use Smiirl\Counter;
 $counter = new Counter("mac-address", "access-token");
-
+$resetExample = $counter->reset();
+var_dump($resetExample);
 $pushExample = $counter->push(12345);
 var_dump($pushExample);
+$addExample = $counter->add(21);
+var_dump($addExample);
